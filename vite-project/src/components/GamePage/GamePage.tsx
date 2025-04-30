57b5799b70f12b7f3e035960dc8633835a285fcc
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-05-01 04:05:24 +0530

    wip- add Header, LandingPage, SettingsPage, GamePage components

---
+++ vite-project/src/components/GamePage/GamePage.tsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+function GamePage() {
+    return (
+        <div>
+            <Header />
+            <p>Game Page</p>
+        </div>
+    );
+}
+
+export default GamePage;
---
+++ vite-project/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+function LandingPage() {
+    return (
+        <div>
+            <Header />
+            <p>Landing Page</p>
+        </div>
+    );
+}
+
+export default LandingPage;
