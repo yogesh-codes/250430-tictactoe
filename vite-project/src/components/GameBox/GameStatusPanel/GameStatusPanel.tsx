84683d62b395a485db66b78d14290b92472788ae
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-05-03 07:29:44 +0530

    wip-work on square-marking logic

--- vite-project/src/components/GameBox/GameStatusPanel/GameStatusPanel.tsx
+++ vite-project/src/components/GameBox/GameStatusPanel/GameStatusPanel.tsx
@@ -1,56 +1,53 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import { useGameContext } from "../../../context/GameContext/GameContext";
 
 function GameStatusPanel() {
-    const gameContext = useGameContext();
+    const { gameStatus, currentPlayer, winnerPlayer } = useGameContext();
 
     let panelMessage: ReactElement | null = null;
-    useEffect(() => {
-        console.log("came here");
-        switch (gameContext.gameStatus) {
-            case "inProgress": {
-                panelMessage = (
-                    <div>
-                        <h3 className="text-center">
-                            {gameContext.currentPlayer.playerName}'s Turn
-                        </h3>
-                        <h4 className="text-center">
-                            Mark {gameContext.currentPlayer.mark}
-                        </h4>
-                    </div>
-                );
-                break;
-            }
 
-            case "restarted": {
-                panelMessage = (
-                    <div>
-                        <h3 className="text-center">Game Started</h3>
-                    </div>
-                );
-                break;
-            }
-            case "completedWin": {
-                panelMessage = (
-                    <div>
-                        <h3 className="text-center">Game Complete</h3>
-                        <h4 className="text-center">
-                            {gameContext.winnerPlayer?.playerName} has Won!
-                        </h4>
-                    </div>
-                );
-                break;
-            }
-            case "completedDraw": {
-                panelMessage = (
-                    <div>
-                        <h3 className="text-center">Game Draw</h3>
-                    </div>
-                );
-                break;
-            }
+    console.log("came here");
+    switch (gameStatus) {
+        case "inProgress": {
+            panelMessage = (
+                <div>
+                    <h3 className="text-center">
+                        {currentPlayer.playerName}'s Turn
+                    </h3>
+                    <h4 className="text-center">Mark {currentPlayer.mark}</h4>
+                </div>
+            );
+            break;
         }
-    }, [gameContext.gameStatus]);
+
+        case "restarted": {
+            panelMessage = (
+                <div>
+                    <h3 className="text-center">Game Started</h3>
+                </div>
+            );
+            break;
+        }
+        case "completedWin": {
+            panelMessage = (
+                <div>
+                    <h3 className="text-center">Game Complete</h3>
+                    <h4 className="text-center">
+                        {winnerPlayer?.playerName} has Won!
+                    </h4>
+                </div>
+            );
+            break;
+        }
+        case "completedDraw": {
+            panelMessage = (
+                <div>
+                    <h3 className="text-center">Game Draw</h3>
+                </div>
+            );
+            break;
+        }
+    }
 
     return panelMessage;
 }
